92410f9a7a99586313ad663f7af2538c86592001
Bruno Negri <b.negri_@hotmail.com>
2022-06-29 10:52:38 -0300

    test github req 4

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 class Game extends React.Component {
-  componentDidMount() {
-
-  }
-
   render() {
     return <p>Games</p>;
   }
 }
 
-export default connect()(Game);
+export default Game;
